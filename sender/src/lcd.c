583d9f10b9bcc14bf2b570e8325380381b7c9206
hurshik7 <hurshik7@gmail.com>
2022-11-27 17:35:28 -0800

    remove warnings

--- sender/src/lcd.c
+++ sender/src/lcd.c
@@ -1,15 +1,17 @@
 #include "lcd.h"
 
+#define FIVE_MILLI_SECONDS (5)
+
 static int fd;                              // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 static int BLEN = 1;                        // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
-static int LCDAddr = 0x27;                  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables, readability-magic-numbers)
+static int LCDAddr = 0x27;                  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables, readability-magic-numbers, cppcoreguidelines-avoid-magic-numbers)
 
 void write_word(int data){
     int temp = data;
     if (BLEN == 1) {
-        temp |= 0x08;
+        temp |= 0x08;                       // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     } else {
-        temp &= 0xF7;
+        temp &= 0xF7;                       // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     }
     wiringPiI2CWrite(fd, temp);
 }
@@ -17,53 +19,53 @@ void write_word(int data){
 void send_command(int comm){
     int buf;
     // Send bit7-4 firstly
-    buf = comm & 0xF0;
-    buf |= 0x04;                    // RS = 0, RW = 0, EN = 1
+    buf = comm & 0xF0;              // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    buf |= 0x04;                    //NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     write_word(buf);
     delay(2);
-    buf &= 0xFB;                    // Make EN = 0
+    buf &= 0xFB;                    // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     write_word(buf);
 
     // Send bit3-0 secondly
-    buf = (comm & 0x0F) << 4;
-    buf |= 0x04;                    // RS = 0, RW = 0, EN = 1
+    buf = (comm & 0x0F) << 4;       // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    buf |= 0x04;                    // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     write_word(buf);
     delay(2);
-    buf &= 0xFB;                    // Make EN = 0
+    buf &= 0xFB;                    // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     write_word(buf);
 }
 
 void send_data(int data){
     int buf;
     // Send bit7-4 firstly
-    buf = data & 0xF0;
-    buf |= 0x05;                    // RS = 1, RW = 0, EN = 1
+    buf = data & 0xF0;              // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    buf |= 0x05;                    // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     write_word(buf);
     delay(2);
-    buf &= 0xFB;                    // Make EN = 0
+    buf &= 0xFB;                    // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     write_word(buf);
 
     // Send bit3-0 secondly
-    buf = (data & 0x0F) << 4;
-    buf |= 0x05;                    // RS = 1, RW = 0, EN = 1
+    buf = (data & 0x0F) << 4;       // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    buf |= 0x05;                    // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     write_word(buf);
     delay(2);
-    buf &= 0xFB;                    // Make EN = 0
+    buf &= 0xFB;                    // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     write_word(buf);
 }
 
 void init_lcd(void) {
 	fd = wiringPiI2CSetup(LCDAddr);
-    send_command(0x33);     // Must initialize to 8-line mode at first
-    delay(5);
-    send_command(0x32);     // Then initialize to 4-line mode
-    delay(5);
-    send_command(0x28);     // 2 Lines & 5*7 dots
-    delay(5);
-    send_command(0x0C);     // Enable display without cursor
-    delay(5);
-    send_command(0x01);     // Clear Screen
-    wiringPiI2CWrite(fd, 0x08);
+    send_command(0x33);     // Must initialize to 8-line mode at first //NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    delay(FIVE_MILLI_SECONDS);
+    send_command(0x32);     // Then initialize to 4-line mode           // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    delay(FIVE_MILLI_SECONDS);
+    send_command(0x28);     // 2 Lines & 5*7 dots                       // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    delay(FIVE_MILLI_SECONDS);
+    send_command(0x0C);     // Enable display without cursor            // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    delay(FIVE_MILLI_SECONDS);
+    send_command(0x01);     // Clear Screen                             // NOLINT(hicpp-signed-bitwise,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+    wiringPiI2CWrite(fd, 0x08);                                         // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
 }
 
 void lcd_clear(void) {
@@ -71,13 +73,14 @@ void lcd_clear(void) {
 }
 
 void lcd_write(int x, int y, const char data[]) {
-    int addr, i;
+    int addr;
+    int i;
     int tmp;
     if (x < 0) {
         x = 0;
     }
-    if (x > 15) {
-        x = 15;
+    if (x > 15) {                               // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+        x = 15;                                 // NOLINT(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
     }
     if (y < 0) {
         y = 0;
