b212d51fc6d59eeb17ffc701f415977771c41b42
hurshik7 <hurshik7@gmail.com>
2022-11-30 19:04:31 -0800

    remove unused code

--- guessing_server/src/led.c
+++ guessing_server/src/led.c
@@ -1,13 +1,13 @@
 
 #include "led.h"
 
-void ledInit(void){
+void ledInit(void) {
     softPwmCreate(LedPinRed,  0, INIT_VAL);
     softPwmCreate(LedPinGreen,0, INIT_VAL);
     softPwmCreate(LedPinBlue, 0, INIT_VAL);
 }
 
-void ledColorSet(uchar r_val, uchar g_val, uchar b_val){
+void ledColorSet(uchar r_val, uchar g_val, uchar b_val) {
     softPwmWrite(LedPinRed,   r_val);
     softPwmWrite(LedPinGreen, g_val);
     softPwmWrite(LedPinBlue,  b_val);
